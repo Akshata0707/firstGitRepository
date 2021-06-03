355a8b7a446921faeaae03036671c485d1dbe298
M1064286 <M1064286@MT-PG02AGZ2>
2021-06-03 07:43:56 +0530

    first commit

---
+++ Git/src/git/GitFirst.java
@@ -0,0 +1,22 @@
+package git;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+public class GitFirst {
+
+	public static void main(String[] args) {
+		Map<String, String> lead = new TreeMap<String, String>();
+		lead.put("M1064010", "Ravinder Kumar");
+		lead.put("M10640002", "Ravinder Kumar");
+		lead.put("M10640003", "Ravinder Kumar");
+		lead.put("M10640001", "Ravinder Kumar");
+
+		for (Entry<String, String> e : lead.entrySet()) {
+			System.out.println(e.getKey() + " : " + e.getValue());
+		}
+		// System.out.println(lead);
+
+	}
+}
